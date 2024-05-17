a23833e5f6f454101f950e2529247dbbd5b8730e
s1arkey <jas102@geneseo.edu>
2024-05-16 22:42:45 -0400

    Project

--- Untitled.R
+++ Untitled.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-data <- read_csv('yfinance-history.csv')
+stock_data <- read_csv('yfinance-history.csv')
 data2 <- read_csv('yfinance-income-stmt.csv')
 data3 <- read_csv('yfinance-balance-sheet.csv')
 
